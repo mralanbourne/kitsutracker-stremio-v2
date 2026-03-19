fed71de77f38edcba221e151eab2de9b7958a4d5
(ᇴ‿ฺᇴ) <spam-born@inbox.lv>
2026-03-19 16:19:24 +0100

    Add Ram Caching for Database Optimization

--- app/services/db.py
+++ app/services/db.py
@@ -4,10 +4,14 @@ from datetime import datetime, timedelta
 from typing import Optional
 from quart import current_app
 from config import Config
+from cachetools import TTLCache
 
 logger = logging.getLogger(__name__)
 
 headers = {"Authorization": f"Bearer {Config.UPSTASH_REDIS_REST_TOKEN}"}
+
+# RAM-Cache:
+user_cache = TTLCache(maxsize=1000, ttl=300)
 
 async def _redis_request(method: str, endpoint: str, payload: str = None):
     url = f"{Config.UPSTASH_REDIS_REST_URL}/{endpoint}"
@@ -26,6 +30,12 @@ async def _redis_request(method: str, endpoint: str, payload: str = None):
         return {}
 
 async def get_user(user_id: str) -> Optional[dict]:
+    
+    if user_id in user_cache:
+       
+        return user_cache[user_id].copy()
+        
+    
     res = await _redis_request("GET", f"get/user:{user_id}")
     data = res.get("result")
 
@@ -37,6 +47,10 @@ async def get_user(user_id: str) -> Optional[dict]:
 
         if "last_updated" in user and isinstance(user["last_updated"], str):
             user["last_updated"] = datetime.fromisoformat(user["last_updated"])
+            
+        
+        user_cache[user_id] = user.copy()
+        
         return user
     except Exception as e:
         logger.error(f"Error when trying to parse user data: {e}")
@@ -54,16 +68,19 @@ async def store_user(user_details: dict) -> bool:
     payload_string = json.dumps(data_to_store)
     
     res = await _redis_request("POST", f"set/user:{user_id}", payload=payload_string)
-    return res.get("result") == "OK"
+    success = res.get("result") == "OK"
+    
+    if success:
+        user_cache[user_id] = user_details.copy()
+        
+    return success
 
-async def update_user_progress(user_id: str, anime_id: str, episode: int):
-    user = await get_user(user_id)
-    if user:
-        if "progress" not in user:
-            user["progress"] = {}
-            
-        user["progress"][str(anime_id)] = episode
-        await store_user(user)
+async def update_user_progress(user: dict, anime_id: str, episode: int):
+    if "progress" not in user:
+        user["progress"] = {}
+        
+    user["progress"][str(anime_id)] = episode
+    await store_user(user)
 
 async def get_valid_user(user_id: str) -> tuple[dict, Optional[str]]:
     user = await get_user(user_id)
